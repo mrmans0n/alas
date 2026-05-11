3c07e51fd14ac44f23779ab534fe0ff34feaf8b2
Nacho Lopez <nacho@nlopez.io>
2026-05-11 21:02:23 +0200

    feat(git): add CommitDetails and CommitChangedFile types

---
+++ Alas/Sources/Git/CommitDetails.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct CommitDetails: Equatable {
+    let info: CommitInfo
+    let body: String          // commit message minus the subject line; "" if none
+    let authorEmail: String
+    let parents: [String]     // short shas of parent commits, in git order
+    let files: [CommitChangedFile]
+}
+
+struct CommitChangedFile: Identifiable, Equatable, Hashable {
+    var id: String { path }
+    let path: String          // for renames, this is the new path
+    let status: String        // single letter: A, M, D, R, C, T
+    let add: Int              // 0 for binary files
+    let del: Int              // 0 for binary files
+}
