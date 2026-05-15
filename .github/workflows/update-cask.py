29fd4db5c1109000442ce63fe5acf39def6dc165
Nacho Lopez <nacho@nlopez.io>
2026-05-15 17:29:30 +0200

    ci: distribute Alas via Homebrew cask on tagged releases (#120)
    * ci: add update-cask.py helper for homebrew cask generation
    
    * ci: include version + arch in release artifact names
    
    * ci: publish homebrew cask on stable releases

---
+++ .github/workflows/update-cask.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Generate the alas Homebrew cask file."""
+import os
+import pathlib
+
+VERSION = os.environ["VERSION"]
+SHA_ARM = os.environ["SHA_ARM"]
+
+CASK = f'''\
+cask "alas" do
+  version "{VERSION}"
+  sha256 "{SHA_ARM}"
+
+  url "https://github.com/mrmans0n/alas/releases/download/v#{{version}}/Alas-#{{version}}-arm64.dmg"
+  name "Alas"
+  desc "AI parallel agent orchestrator"
+  homepage "https://github.com/mrmans0n/alas"
+
+  depends_on arch: :arm64
+  depends_on macos: :sonoma
+
+  app "Alas.app"
+
+  zap trash: [
+    "~/Library/Application Support/Alas",
+    "~/Library/Caches/io.nlopez.alas",
+    "~/Library/HTTPStorages/io.nlopez.alas",
+    "~/Library/Preferences/io.nlopez.alas.plist",
+    "~/Library/Saved Application State/io.nlopez.alas.savedState",
+    "~/Library/WebKit/io.nlopez.alas",
+  ]
+end
+'''
+
+pathlib.Path("Casks/alas.rb").write_text(CASK)
+print(f"Updated cask to {VERSION}")
