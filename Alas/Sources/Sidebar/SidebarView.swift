04038d6480d53764dc055bdbeaa7aff81b504826
Nacho Lopez <nacho@nlopez.io>
2026-06-01 16:46:18 +0200

    fix(worktrees): persist reset-to-default via saveProjects

--- Alas/Sources/Sidebar/SidebarView.swift
+++ Alas/Sources/Sidebar/SidebarView.swift
@@ -61,7 +61,10 @@ struct SidebarView: View {
                                 onNewWorktree: { onNewWorktree(project.id) },
                                 onEditProject: { onEditProject(project.id) },
                                 onRemoveProject: { onRemoveProject(project.id) },
-                                onResetSort: { state.projectsManager.resetWorktreeOrder(projectId: project.id) },
+                                onResetSort: {
+                                    state.projectsManager.resetWorktreeOrder(projectId: project.id)
+                                    state.saveProjects()
+                                },
                                 spaces: state.spacesManager.spaces,
                                 activeSpaceId: state.spacesManager.activeSpaceId,
                                 isProjectInSpace: { spaceId in
